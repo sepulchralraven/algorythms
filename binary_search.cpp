66f56655065b8a3c7e73f39aaff4b528f965f908
leisa <55181930+sepulchralraven@users.noreply.github.com>
2023-08-18 14:33:05 +0300

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,37 +1,44 @@
+//how to define the size of array in function??
+//change the types of input parameters (maybe user wants to search char types of data)
+
 #include <stdio.h>
 #include <iostream>
+#include <array>
 using namespace std;
 
-//testing branch additional
-
-int binary_search (int searchable)
+int
+binary_search (int *arr, int searchable)
 {
-  int n = 10;
-  int *arr = new int[n];
-  for (int i = 0; i < n; i++) 
-  {
-      arr[i] = i + 1;
-  }
+  int n = 100;
   int low = 0;
   int high = n;
   int mid = (high + low) / 2;
   int guess = arr[mid];
-  
-  while (guess != searchable)
+
+  while (low <= high)
     {
-      if (guess > searchable)
-	high = mid;
-      else
-	low = mid;
       mid = (high + low) / 2;
       guess = arr[mid];
+      if (guess > searchable)
+	high = mid -1 ;
+      if (guess < searchable)
+	low = mid + 1;
+      if (guess == searchable)
+	return mid;
     }
-  return mid;
+  return NULL;
 }
 
-int main ()
+int
+main ()
 {
-  int index = binary_search(8);
+  int n = 100;
+  int *arr = new int[n];
+  for (int i = 0; i < n; i++)
+    {
+      arr[i] = i + 1;
+    }
+  int index = binary_search (arr, 450);
   cout << "The index of serchable number in binary search is " << index;
   return 0;
 }
